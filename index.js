31d7b917f06629fa0b179d6ff27e5dbca09f09a7
Hilton Shumway <hillshum@gmail.com>
2017-06-14 18:03:37 -0600

    Mostly works.
    The blasted string won't render properly

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const {ratePost} = require('./lib/rateHandlers')
 const app = express();
+
+const calculateRate = require('./lib/calculateRate')
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,11 +19,12 @@ app.get('/rate', function(request, response) {
   response.render('pages/rateForm');
 });
 
-app.post('/rate', ratePost)
 
-app.get('/results', (req, res) => {
-  res.render('pages/results')
-})
+app.get('/results', calculateRate) //, (req, res)=> {
+//   console.log(typeof res.rate)
+//   res.locals.blahe = res.rate
+//   res.render('pages/results' )
+// })
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
